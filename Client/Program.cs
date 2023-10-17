ea51ab397f5b3ba60b3ded97673ddb8a83eed933
TalonEzio <vuthemanh1707@gmail.com>
2023-10-17 19:06:19 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static readonly IPAddress IpAddress = IPAddress.Loopback;//IPAddress.Parse("127.0.0.1")
+        private static readonly int Port = 100;
+        static void Main()
+        {
+            Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Client");
+
+            Thread thread = new Thread(() =>
+            {
+                LoopFunction(IpAddress, Port);
+            });
+            thread.Start();
+
+        }
+
+        private static void LoopFunction(IPAddress ipAddress, int port)
+        {
+            while (true)
+            {
+                Console.Write("Mời nhập ký tự: ");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("xESC");//fake char
+                    SendToServer(ipAddress, port, keyInfo.KeyChar);
+                    Console.Read();
+                    break;
+                }
+
+                Console.WriteLine();
+                SendToServer(ipAddress, port, keyInfo.KeyChar);
+            }
+        }
+
+        private static void SendToServer(IPAddress ipAddress, int port, char key)
+        {
+            var serverEndPoint = new IPEndPoint(ipAddress, port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(serverEndPoint);
+                var sendBuffer = new[] { (byte)key };
+                socket.Send(sendBuffer);
+
+                socket.Shutdown(SocketShutdown.Send);
+
+
+                var binDataIn = new byte[512];
+
+                var length = socket.Receive(binDataIn);
+
+                var receiveBuffer = Encoding.Unicode.GetString(binDataIn, 0, length);
+                //Console.WriteLine();
+                Console.WriteLine(receiveBuffer);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static readonly IPAddress IpAddress = IPAddress.Loopback;
+        private static readonly int Port = 100;
+
+        static void Main()
+        {
+            Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Server");
+            TcpListener myListener = new TcpListener(IpAddress, Port);
+
+            Thread thread = new Thread(
+                () =>
+                {
+                    HandleClient(myListener);
+                }
+                );
+            thread.Start();
+        }
+
+        static void HandleClient(TcpListener myListener)
+        {
+            try
+            {
+                myListener.Start();
+
+                Console.WriteLine("Server đang chạy ở : " + myListener.LocalEndpoint);
+                Console.WriteLine("Đang chờ kết nối.....");
+
+
+                while (true)
+                {
+                    Socket socket = myListener.AcceptSocket();
+
+                    Console.WriteLine("Kết nối từ " + socket.RemoteEndPoint);
+
+                    byte[] binDataIn = new byte[512];
+                    socket.Receive(binDataIn);
+
+                    var receive = Encoding.Unicode.GetString(binDataIn)[0];
+                    var receiveView = receive == '\u001b' ? "Esc" : receive.ToString();
+                    Console.WriteLine($"Dữ liệu gửi từ client: {receiveView}");
+                    byte[] binOut;
+                    if (receive == '\u001b')
+                    {
+                        binOut = Encoding.Unicode.GetBytes("Đã nhận lệnh dừng chương trình");
+                        socket.Send(binOut);
+                        Console.WriteLine("Dừng chương trình!");
+                        socket.Close();
+                        break;
+                    }
+
+                    binOut = Encoding.Unicode.GetBytes($"Đã nhận được {receive} từ client");
+                    socket.Send(binOut);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
